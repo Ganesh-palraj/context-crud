df842c1e0dedfed51d63d735e6347b5ec08f455d
ganesh <jensendavid007@outlook.com>
2023-10-31 22:09:56 +0530

    error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Action from "./components/Action";
-
+import Action from "./components/Action"
 import "bootstrap/dist/css/bootstrap.min.css";
-//import Students from "./components/Home";
 import Home from "./components/Home";
 import Teachers from "./components/Teachers";
-import Teacheraction from "./components/Teacheraction";
+import Teacheraction from "./components/Teacheraction"
 import Provider from "./Provider";
 
 function App() {
@@ -17,15 +15,9 @@ function App() {
           <Sidebar />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
-              path="/teachers"
-              element={<Teachers  />}
-            />
+            <Route path="/teachers" element={<Teachers />} />
             <Route path="/action/" element={<Action />} />
-            <Route
-              path="/teacheraction/"
-              element={<Teacheraction />}
-            />
+            <Route path="/teacheraction/" element={<Teacheraction />} />
             <Route path="/action/:id" element={<Action />} />
             <Route path="/teacheraction/:id" element={<Teacheraction />} />
           </Routes>
